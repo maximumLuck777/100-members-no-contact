a62b733bb6f7d548173e0464ff1efa9d5e4cb8e6
turtletennis <stephenjenkins42@gmail.com>
2026-06-21 13:47:38 +0100

    Add audio_manager global, for playing SFX and music. (#38)

--- Actual Game Folder/scripts/player.gd
+++ Actual Game Folder/scripts/player.gd
@@ -6,18 +6,22 @@ class_name Player
 
 # Can be part of a power up later
 # What's the mechanic like? I'm not sure about the core mechanic right now
-# Is player going to control the beyblade? Against who? It could be speedrun type game.
+# Is player going to control the B0yblade? Against who? It could be speedrun type game.
 
 # export these values to make it easier to adjust
 # we can tweek these until the game feels fun
 const SPARKS_SCENE = preload("res://Actual Game Folder/scenes/components/sparks.tscn")
 
 
+@export_category("Statistics")
 @export var starting_spin_velocity:float = 30
 @export var default_velocity: float = 20
 @export var spin_velocity_drop_on_collision: float = 1
 @export var spin_velocity_drop_over_time: float = 1
 
+@export_category("Resources")
+@export var launch_sfx_stream : AudioStream
+@export var collision_sfx_stream : AudioStream
 @onready var spin_bar: ProgressBar = $CanvasLayer/SpinBar
 
 
@@ -26,6 +30,8 @@ var spin_velocity: float = starting_spin_velocity
 var player_died: bool = false
 
 func _ready() -> void:
+	AudioManager.play_sfx(launch_sfx_stream,global_position)
+
 	# this is necessary for _on_body_entered, 1 is technically enough for just the player but with multiple bayblades we might need to increase this value.
 	max_contacts_reported = 5
 
@@ -61,8 +67,11 @@ func _physics_process(delta: float) -> void:
 
 # slightly lower spin velocity every time there is a collision with another rigid body
 # we can add ways to increase your spin later to give the player more control
-func _on_body_entered(_body: Node) -> void:
+func _on_body_entered(body: Node) -> void:
 	spin_velocity -= spin_velocity_drop_on_collision
+	if(body is Node2D):
+		var body2D = body as Node2D
+		AudioManager.play_sfx(collision_sfx_stream, body2D.global_position)
 	pass
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
